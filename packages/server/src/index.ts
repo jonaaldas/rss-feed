237c79016ba25599425a14ccda0e926750e9c5a3
Jonathan Aldas <jonaaldas@gmail.com>
2025-10-16 00:22:10 +0200

    serving the frontend from hono

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,12 +1,13 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 const router = new Hono();
 
-router.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+router.get("*", serveStatic({ root: "./frontend/dist" }));
+router.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
 
 export type AppType = typeof router;
+
 export default {
-  port: 3000,
+  port: 9595,
   fetch: router.fetch,
 };
