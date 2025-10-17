4d805bea4d8be753a131d66b592041ae817c44ae
Jonathan Aldas <jonaaldas@gmail.com>
2025-10-17 19:36:40 +0200

    vue router installed

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -2,10 +2,11 @@ import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import VueRouter from "unplugin-vue-router/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), tsconfigPaths()],
+  plugins: [VueRouter(), vue(), tailwindcss(), tsconfigPaths()],
   server: {
     proxy: {
       "/api": {
