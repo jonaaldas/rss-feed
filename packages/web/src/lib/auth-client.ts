43131bf9bacd8dce2feacadbbed197e5fd5347f7
Jonathan Aldas <jonaaldas@gmail.com>
2025-10-19 15:30:22 +0200

    new env

--- packages/web/src/lib/auth-client.ts
+++ packages/web/src/lib/auth-client.ts
@@ -1,6 +1,10 @@
 import { createAuthClient } from "better-auth/vue";
+const baseURL =
+  import.meta.env.VITE_NODE_ENV === "production"
+    ? "http://65.109.128.10:9595/api/auth"
+    : "http://localhost:5173/api/auth";
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:5173/api/auth",
+  baseURL,
 });
 
 export const { signIn, signUp, useSession } = authClient;
--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,33 +1,35 @@
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import VueRouter from "unplugin-vue-router/vite";
 import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [VueRouter(), vue(), tailwindcss(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:9595",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [VueRouter(), vue(), tailwindcss(), tsconfigPaths()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
-  build: {
-    outDir: "../server/frontend/dist",
-    rollupOptions: {
-      output: {
-        // Don't generate source maps for production
-        sourcemap: false,
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:9595",
+          changeOrigin: true,
+        },
       },
     },
-  },
+    build: {
+      outDir: "../server/frontend/dist",
+      rollupOptions: {
+        output: {
+          sourcemap: false,
+        },
+      },
+    },
+  };
 });
